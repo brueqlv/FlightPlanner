2ef9c2b1199a0af3480c34a68ab46c8eb517c755
brueqlv <gundarens@gmail.com>
2024-02-15 10:15:48 +0200

    Added CleanupApiController

---
+++ FlightPlanner/Controllers/CleanupApiController.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.Design.Serialization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanner.Controllers
+{
+    [Route("testing-api")]
+    [ApiController]
+    public class CleanupApiController : ControllerBase
+    {
+        [HttpPost]
+        [Route("clear")]
+        public IActionResult Clear()
+        {
+            return Ok();  //Jāpievieno funkcionalitāte
+        }
+    }
+}
