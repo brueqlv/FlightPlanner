ae5dd3b6103334019f0406cc581f1e9010a6bd0f
brueqlv <gundarens@gmail.com>
2024-02-20 09:16:50 +0200

    Deleted comments

--- FlightPlanner/Program.cs
+++ FlightPlanner/Program.cs
@@ -8,14 +8,12 @@ using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
 
 builder.Services.AddAuthentication("BasicAuthentication")
     .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -32,7 +30,6 @@ builder.Services.AddDbContext<FlightDbContext>(option =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
